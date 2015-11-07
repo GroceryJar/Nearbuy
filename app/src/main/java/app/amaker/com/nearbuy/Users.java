3ab592d55752f90742564c961d826036f9c51515
Rachel Xu <rachel960122@hotmail.com>
2015-11-07 13:12:50 -0500

    fix bugs

--- app/src/main/java/app/amaker/com/nearbuy/Users.java
+++ app/src/main/java/app/amaker/com/nearbuy/Users.java
@@ -16,9 +16,9 @@ public final class Users {
         public static final String COLUMN_NAME_LAST_NAME = "last name";
         public static final String COLUMN_NAME_LINKURI = "link";
 
-        private static final String TEXT_TYPE = " TEXT";
-        private static final String COMMA_SEP = ",";
-        private static final String SQL_CREATE_ENTRIES =
+        public static final String TEXT_TYPE = " TEXT";
+        public static final String COMMA_SEP = ",";
+        public static final String SQL_CREATE_ENTRIES =
                 "CREATE TABLE " + UserEntry.TABLE_NAME + " (" +
                         UserEntry._ID + " INTEGER PRIMARY KEY," +
                         UserEntry.COLUMN_NAME_USER_ID + TEXT_TYPE + COMMA_SEP +
@@ -26,34 +26,33 @@ public final class Users {
                         UserEntry.COLUMN_NAME_LAST_NAME + TEXT_TYPE + COMMA_SEP +
                         UserEntry.COLUMN_NAME_LINKURI + TEXT_TYPE + COMMA_SEP + " )";
 
-        private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + UserEntry.TABLE_NAME;
+        public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + UserEntry.TABLE_NAME;
 
-        public class UsersDbHelper extends SQLiteOpenHelper {
-            public static final int DATABASE_VERSION = 1;
-            public static final String DATABASE_NAME = "users.db";
+    }
+    public class UsersDbHelper extends SQLiteOpenHelper {
+        public static final int DATABASE_VERSION = 1;
+        public static final String DATABASE_NAME = "users.db";
 
-            public UsersDbHelper(Context context) {
-                super(context, DATABASE_NAME, null, DATABASE_VERSION);
-            }
+        public UsersDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
 
-            @Override
-            public void onCreate(SQLiteDatabase db) {
-                db.execSQL(SQL_CREATE_ENTRIES);
-            }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(UserEntry.SQL_CREATE_ENTRIES);
+        }
 
-            @Override
-            public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-                // This database is only a cache for online data, so its upgrade policy is
-                // to simply to discard the data and start over
-                db.execSQL(SQL_DELETE_ENTRIES);
-                onCreate(db);
-            }
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // This database is only a cache for online data, so its upgrade policy is
+            // to simply to discard the data and start over
+            db.execSQL(UserEntry.SQL_DELETE_ENTRIES);
+            onCreate(db);
+        }
 
-            @Override
-            public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-                onUpgrade(db, oldVersion, newVersion);
-            }
-
+        @Override
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
         }
     }
 }
