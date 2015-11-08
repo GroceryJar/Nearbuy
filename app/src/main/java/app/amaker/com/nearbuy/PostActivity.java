d96a9cab56f95ab184a0fe189409a73ceb5b2f58
Rachel Xu <rachel960122@hotmail.com>
2015-11-07 19:03:43 -0500

    add 'addPost' method'

--- app/src/main/java/app/amaker/com/nearbuy/PostActivity.java
+++ app/src/main/java/app/amaker/com/nearbuy/PostActivity.java
@@ -1,16 +1,65 @@
 package app.amaker.com.nearbuy;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.facebook.Profile;
+
+import java.util.Date;
 
 public class PostActivity extends ActionBarActivity {
+    EditText item_name;
+    EditText item_description;
+    EditText item_price;
+    Button submit_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        item_name = (EditText) findViewById(R.id.item_name);
+        item_description = (EditText) findViewById(R.id.item_description);
+        item_price = (EditText) findViewById(R.id.item_price);
+        submit_button = (Button) findViewById(R.id.button_post);
+
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+    }
+
+    private void addPost() {
+        Profile p = Profile.getCurrentProfile();
+
+        String itna = item_name.getText().toString();
+        String itdes = item_description.getText().toString();
+        Double itpr = Double.parseDouble(item_price.getText().toString());
+        String createTime = new Date().toString();
+        String updateTime = new Date().toString();
+
+        Posts.PostsDbHelper pDbHelper = new Posts().new PostsDbHelper(getApplicationContext());
+        SQLiteDatabase db = pDbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Posts.PostEntry.COLUMN_NAME_FIRST_NAME, p.getFirstName());
+        values.put(Posts.PostEntry.COLUMN_NAME_LAST_NAME, p.getLastName());
+        values.put(Posts.PostEntry.COLUMN_NAME_ITEM_NAME, itna);
+        values.put(Posts.PostEntry.COLUMN_NAME_DESCRIPTION, itdes);
+        values.put(Posts.PostEntry.COLUMN_NAME_PRICE, itpr);
+
+        long newRowId;
+        newRowId = db.insert(Posts.PostEntry.TABLE_NAME, null, values);
+
     }
 
     @Override
